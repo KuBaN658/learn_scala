8e4ded283010c228ae48b2df129356b6ae5550ac
KuBaN_ <garkovski_dmitri@mail.ru>
2023-05-22 14:02:20 +0300

    деревья осталась одна задача

--- src/exercises/tree.scala
+++ src/exercises/tree.scala
@@ -1,5 +1,5 @@
 package exercises
-
+import scala.annotation.tailrec
 object tree extends App {
   abstract class BinaryTree[+T] {
     def value: T
@@ -10,6 +10,8 @@ object tree extends App {
     def collectLeaves: List[BinaryTree[T]]
     def countLeaves: Int
     def nodesAtLevel(level: Int): List[BinaryTree[T]]
+    def collectNodes(): List[T]
+    def hasPath(tree: BinaryTree[Int], target: Int): Boolean
   }
 
   case object TreeEnd extends BinaryTree[Nothing] {
@@ -21,6 +23,8 @@ object tree extends App {
     override def collectLeaves: List[BinaryTree[Nothing]] = List()
     override def countLeaves: Int = 0
     override def nodesAtLevel(level: Int) = List():List[BinaryTree[Nothing]]
+    override def collectNodes() = List(): List[Nothing]
+    def hasPath(tree: BinaryTree[Int], target: Int): Boolean = false
   }
 
   case class Node[+T](
@@ -31,6 +35,7 @@ object tree extends App {
     override def isEmpty: Boolean = false
     override def isLeaf: Boolean = leftChild.isEmpty && rightChild.isEmpty
     override def collectLeaves: List[BinaryTree[T]] = {
+      @tailrec
       def loop(tr: BinaryTree[T] = this,
                setNode: Set[BinaryTree[T]] = Set(),
                acc: List[BinaryTree[T]] = List()): List[BinaryTree[T]] = {
@@ -43,7 +48,7 @@ object tree extends App {
     }
     override def countLeaves: Int = this.collectLeaves.length
     override def nodesAtLevel(level: Int): List[BinaryTree[T]] = {
-      @scala.annotation.tailrec
+      @tailrec
       def loop(level: Int, acc: List[BinaryTree[T]] = List(this)): List[BinaryTree[T]] = {
         if (level == 0 || acc.isEmpty) acc
         else {
@@ -54,7 +59,32 @@ object tree extends App {
       }
       loop(level)
     }
+    override def collectNodes(): List[T] = {
+      @tailrec
+      def loop(acc: List[BinaryTree[T]] = List(this), lastLevel: List[BinaryTree[T]] = List(this)): List[T] = {
+        val n_acc = lastLevel.flatMap(elem => List(elem.leftChild, elem.rightChild)).filterNot(_.isEmpty)
+        if (n_acc.isEmpty) acc.map(_.value)
+        else loop(acc ++ n_acc, n_acc)
+      }
+      loop()
+    }
+    def hasPath(tree: BinaryTree[Int], target: Int): Boolean = {
+      @tailrec
+      def loop(acc: Map[BinaryTree[Int],Int] = Map(tree -> tree.value)): Boolean = {
+        val nAcc: Map[BinaryTree[Int], Int] = acc.filterNot((key: BinaryTree[Int], value: Int) => key.isLeaf)
+          .flatMap((key, value) => (key, value) match {
+            case (key, value) if key.leftChild.isEmpty => Map(key.rightChild -> (value + key.rightChild.value))
+            case (key, value) if key.rightChild.isEmpty => Map(key.leftChild -> (value + key.leftChild.value))
+            case _ => Map(key.leftChild -> (value + key.leftChild.value), key.rightChild -> (value + key.rightChild.value))
+          })
 
+        if (! acc.filter((key, value) => key.isLeaf && value == target).isEmpty) true
+        else if (nAcc.isEmpty) false
+        else loop(nAcc)
+      }
+      if (tree.isEmpty) false
+      else loop()
+    }
   }
 
   val tree = Node(1,
@@ -75,5 +105,13 @@ object tree extends App {
         TreeEnd,
         TreeEnd)))
 
-  println(tree.nodesAtLevel(3))
+  println(Map(tree -> tree.value, Node(5, TreeEnd, Node(8, TreeEnd, TreeEnd)) -> 5)
+    .filterNot((key: BinaryTree[Int], value: Int) => key.isLeaf)
+    .flatMap((key, value) => (key, value) match {
+      case (key, value) if key.leftChild.isEmpty => Map(key.rightChild -> (value + key.rightChild.value))
+      case (key, value) if key.rightChild.isEmpty => Map(key.leftChild -> (value + key.leftChild.value))
+      case _ => Map(key.leftChild -> (value + key.leftChild.value), key.rightChild -> (value + key.rightChild.value))
+    }))
+
+  println(tree.hasPath(TreeEnd, 1))
 }
